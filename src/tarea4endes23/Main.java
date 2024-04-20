adda3d930656a014cdb407741ba372173db5bfdc
aleberlo27 <alejandrabernallopez1@gmial.com>
2024-04-20 13:45:38 +0200

    Version2SEGUNDOCOMMIT

--- src/tarea4endes23/Main.java
+++ src/tarea4endes23/Main.java
@@ -8,14 +8,18 @@ package tarea4endes23;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CCuenta miCuenta;
         double saldoActual;
+        double saldoRetirado;
         
 
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
+        miCuenta.retirar(200.0);
+        saldoRetirado = miCuenta.estado();
+        System.out.println("El saldo despues de retirar es: "+ saldoRetirado);
         
     
     }
